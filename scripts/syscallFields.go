96bd496d784ba855607f63b75fa57e9d8699ddfe
IMElTgp <2827228085@qq.com>
2026-05-19 13:53:38 +0800

    updated text processing scripts that output syscall argument list

---
+++ scripts/syscallFields.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getSyscallFields() error {
+	for syscallID, syscallName := range syscallTable {
+		_ = syscallID
+		f, err := os.Open("/sys/kernel/tracing/events/syscalls/sys_enter_" + syscallName + "/format")
+		if err != nil {
+			fmt.Printf("skipping %s because of %s\n", syscallName, err.Error())
+			continue
+		}
+		defer f.Close()
+
+		var scanner = bufio.NewScanner(f)
+		var seenBlankLine = false
+		fields := make([]string, 0)
+
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+
+			if line == "" {
+				seenBlankLine = true
+				continue
+			}
+			if !seenBlankLine {
+				continue
+			}
+			if !strings.HasPrefix(line, "field:") {
+				continue
+			}
+			line = strings.TrimPrefix(line, "field:")
+			line = strings.TrimSuffix(line, ";")
+			parts := strings.Split(line, ";")
+			line = parts[0]
+
+			var (
+				Type      string
+				fieldName string
+			)
+
+			// may contain multiple white spaces
+			parts = strings.Split(line, " ")
+			if len(parts) < 2 {
+				continue
+			}
+			fieldName = parts[len(parts)-1]
+
+			// consider fields whose names begin with _ as non-user fields
+			if strings.HasPrefix(fieldName, "_") {
+				continue
+			}
+			Type = strings.Join(parts[:len(parts)-1], " ")
+
+			sep := " "
+			if strings.HasSuffix(Type, "*") {
+				sep = ""
+			}
+			fields = append(fields, strings.Join([]string{Type, fieldName}, sep))
+		}
+		fd, err := os.OpenFile("syscall_fields.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+		defer fd.Close()
+		w := bufio.NewWriter(fd)
+		/*
+			w.WriteString(fmt.Sprintf("%d: ", syscallID) + syscallName + "(")
+			for i, field := range fields {
+				w.WriteString(field)
+				if i < len(fields)-1 {
+					w.WriteString(", ")
+				}
+			}
+			w.WriteString(")\n")*/
+		w.WriteString("\"" + syscallName + "\": {\n")
+		for _, field := range fields {
+			w.WriteString("    \"" + field + "\",\n")
+		}
+		w.WriteString("},\n")
+		w.Flush()
+	}
+	return nil
+}
+
+/**
+var syscallFields = make(map[string][]string)
+
+syscallFields = map[string][]string{
+	"read": []string{
+		"unsigned int fd",
+		"char *buf",
+		"size_t count",
+	},
+}
+*/
---
+++ scripts/syscallMapping.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func processSyscallNameToIDMapping() {
+	t, err := getRawText()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	name, id, err := processRawText(t)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for i := range name {
+		f, err := os.OpenFile("syscallNameToID.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		var w = bufio.NewWriter(f)
+		if _, err := w.WriteString(id[i] + ":" + " " + "\"" + name[i] + "\"" + ",\n"); err != nil {
+			fmt.Println(err.Error())
+		}
+		w.Flush()
+		f.Close()
+	}
+}
+
+func getRawText() (string, error) {
+	out, err := exec.Command("sed", "-n", "1,400p", "/usr/include/asm/unistd_64.h").Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to parse: %w", err)
+	}
+
+	return strings.TrimSpace(string(out)), nil
+}
+
+func processRawText(rawText string) (name []string, id []string, err error) {
+	lines := strings.Split(rawText, "\n")
+
+	for _, line := range lines {
+		if !strings.HasPrefix(line, "#define ") {
+			continue
+		}
+
+		line = strings.TrimPrefix(line, "#define ")
+		if !strings.HasPrefix(line, "__NR_") {
+			continue
+		}
+
+		line = strings.TrimPrefix(line, "__NR_")
+
+		before, after, found := strings.Cut(line, " ")
+		if !found {
+			return nil, nil, fmt.Errorf("invalid kernel file format, causing: %w", err)
+		}
+
+		name = append(name, before)
+		id = append(id, after)
+	}
+	return name, id, nil
+}
